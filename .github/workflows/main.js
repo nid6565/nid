39b7e8ced7cfbe4fa2d3c2aa522ccccdcc13579e
nid6565 <mevsmemerug@gmail.com>
2024-10-12 20:25:37 -0400

    Create main.js

---
+++ .github/workflows/main.js
@@ -0,0 +1,36 @@
+!(function ($) {
+  "use strict";
+
+  // ... (Other code remains unchanged)
+
+  // Smooth scroll for the navigation menu and links with .scrollto classes
+  $(document).on('click', '.nav-menu a, .scrollto', function (e) {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      e.preventDefault();
+      var target = $(this.hash);
+      if (target.length) {
+
+        var scrollto = target.offset().top;
+
+        // Stop any ongoing animations before starting a new one
+        $('html, body').stop(true, false).animate({
+          scrollTop: scrollto
+        }, 1500, 'easeInOutExpo');
+
+        if ($(this).parents('.nav-menu, .mobile-nav').length) {
+          $('.nav-menu .active, .mobile-nav .active').removeClass('active');
+          $(this).closest('li').addClass('active');
+        }
+
+        if ($('body').hasClass('mobile-nav-active')) {
+          $('body').removeClass('mobile-nav-active');
+          $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+        }
+        return false;
+      }
+    }
+  });
+
+  // ... (Rest of the code remains unchanged)
+
+})(jQuery);
