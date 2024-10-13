11c3b9d8c5ab37c31c6ca4b8f4403650fc9d1266
nid6565 <mevsmemerug@gmail.com>
2024-10-13 00:52:26 -0400

    Create python.py

---
+++ .github/workflows/python.py
@@ -0,0 +1,79 @@
+# integer assignments (only the last value is relevant)
+a = 4  
+
+# float assignments (only the last value is relevant)
+a = 2.1  
+
+# string assignments
+a = "string"
+name = "Tenzin"
+
+# Boolean assignments (only the last value is relevant)
+a = False  
+
+# Maintain sets as original data structures
+a = {3, 4, 5, 6}
+print(a)
+
+a = {"stringOne", "stringTwo"}
+print(a)
+
+a = {True, False, True}
+print(a)
+
+a = {1.1, 2.2, 3.3}
+print(a)
+
+a = {8, 7, 6, 5}
+print(a)
+
+a = {"stringThree", "stringFour"}
+print(a)
+
+a = {True, False, False}
+print(a)
+
+# Variable assignments
+varA = 1
+vara = 2
+print(vara)
+print(a)
+
+varA = 3  # Updated the value
+vara = varA
+
+var = "string"
+varTwo = var
+print(varTwo)
+
+# Tuple creation
+a_tuple = (a, varA, vara)
+b = [a_tuple, varA, var]
+
+# list operations
+my_list = ["a", "b", "c"]
+print(my_list)
+print(my_list[0])
+
+listVar = 1
+print(listVar)
+
+# string operations
+string = "rinzin"
+print(a_tuple)
+print(string[0])
+
+# Print various values
+print(5)
+print("string")
+print(True)
+print(my_list)
+print(string)
+
+# Logical reassignment
+var = 0  
+name = "rinzin"
+
+# Correct the usage of the existing 'titles' variable
+titles = "rinzin"
+print(titles)  # Corrected variable name from 'title' to 'titles'
